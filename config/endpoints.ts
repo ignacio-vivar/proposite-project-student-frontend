2a25bee1bc9cb18c40bd4058ea753707ca82e55d
Ignacio <jjivc10@gmail.com>
2026-01-06 18:43:37 -0300

    add endpoint backend

--- config/endpoints.ts
+++ config/endpoints.ts
@@ -1,4 +1,11 @@
-export const API_BASE_URL = "http://localhost:8000";
+const isProduction = process.env.NODE_ENV === "production";
+
+export const API_BASE_URL = isProduction
+  ? process.env.NEXT_PUBLIC_API_BASE_URL ||
+  (typeof window !== "undefined"
+    ? window.location.origin
+    : "https://backend-proposite-project.vercel.app")
+  : "http://localhost:8000";
 
 export const LOGIN_URL = `${API_BASE_URL}/auth`;
 
